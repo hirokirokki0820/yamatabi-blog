ce8fc75dc130c17c4f6f23203c5d0928b404a312
hirokirokki0820 <hirokirokki0820@gmail.com>
2023-04-07 16:31:10 +0900

    storage.yml aws の bucket が適用されるよう編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,5 +107,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # config.active_record.sqlite3_production_warning=false
-  config.consider_all_requests_local = true
 end
